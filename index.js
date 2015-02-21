347a355170314ba95f7cdff36692562fce82cbb6
Jeremy Fry <jeremy.m.fry@gmail.com>
2015-02-21 00:03:29 -0500

    Images now resize and gray. Downgraded canvas version due to bug.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+/* global require, console */
 'use strict';
 var express = require('express');
 var serveStatic = require('serve-static');
 var extend = require('extend');
 var fs = require('fs');
-var filesCache = null;
 
 function parseOptionsFromPath(fullPath){
 	var path = fullPath.replace(/\//g, ' ').trim();
@@ -27,6 +27,21 @@ function parseOptionsFromPath(fullPath){
 	return options;
 }
 
+function grayScaleCanvas(ctx, options){
+	var imageData = ctx.getImageData(0, 0, options.width, options.height);
+	var data = imageData.data;
+
+	for(var i = 0; i < data.length; i += 4) {
+		//reduce each color to 33% of it's previous saturation and add them to get grey shade
+		var shadeOfGrey = 0.33 * data[i] + 0.33 * data[i + 1] + 0.33 * data[i + 2];
+		//rgb
+		data[i] = shadeOfGrey;
+		data[i + 1] = shadeOfGrey;
+		data[i + 2] = shadeOfGrey;
+	}
+	ctx.putImageData(imageData, 0, 0);
+}
+
 
 function createImage(options){
 	var Canvas = require('canvas');
@@ -34,19 +49,47 @@ function createImage(options){
 	var canvas = new Canvas(options.width, options.height);
 	var ctx = canvas.getContext('2d');
 
-	//Grab a random image
-	if(!filesCache){
-		filesCache = fs.readdirSync('images');
+	var ratio = options.width/options.height;
+	//Determine with group of photos to pull from
+	var folder = 'n/';
+	if(ratio < 0.8){
+		folder = 't/';
+	}else if(ratio > 1.2){
+		folder = 'w/';
 	}
 
-	var srcImageName = filesCache[Math.floor( Math.random() * filesCache.length)];
-	console.log('images/'+srcImageName);
-	var srcImageBuffer = fs.readFileSync('images/'+srcImageName);
+	//Grab a random image
+	var files = fs.readdirSync('images/'+folder);
+	var srcImageName = files[Math.floor( Math.random() * files.length)];
+	var srcImageBuffer = fs.readFileSync('images/'+folder+srcImageName);
 	var srcImg = new Image;
 	srcImg.src = srcImageBuffer;
-	//return srcImageBuffer;
 
-	ctx.drawImage(srcImg, 0, 0, options.width, options.height);
+	// Scale the photo
+	if(ratio <= srcImg.width/srcImg.height){
+		var resizedWidth = srcImg.width*(options.height/srcImg.height);
+		ctx.drawImage(srcImg,
+			0, 0, srcImg.width, srcImg.height,
+			-(resizedWidth-options.width)/2, 0, resizedWidth, options.height
+		);
+	}else{
+		var resizedHeight = srcImg.height*(options.width/srcImg.width);
+		ctx.drawImage(srcImg,
+			0, 0, srcImg.width, srcImg.height,
+			0, -(resizedHeight-options.height)/2, options.width, resizedHeight
+		);
+	}
+
+	if(options.grayScale){
+		grayScaleCanvas(ctx, options);
+	}
+
+	ctx.font = '25px Verdana';
+	ctx.fillStyle = 'white';
+	ctx.strokeStyle = 'black';
+	ctx.lineWidth = 2;
+	ctx.strokeText(options.width+"x"+options.height, 10, 30);
+	ctx.fillText(options.width+"x"+options.height, 10, 30);
 
 	return canvas.toBuffer();
 }
